9190c377265b94f2e38ec5c4b9574a954fe0e6e3
CDavidDv <carlos.david1806@gmail.com>
2025-01-31 18:13:32 -0600

    un numero xd

--- resources/js/Components/PastesHorneados.vue
+++ resources/js/Components/PastesHorneados.vue
@@ -84,9 +84,6 @@ const grandTotalPiezas = computed(() => {
 const formatDate = (dateString) => {
   const date = new Date(dateString);
   return date.toLocaleString('es-MX', {
-    day: '2-digit',
-    month: '2-digit',
-    year: 'numeric',
     hour: '2-digit',
     minute: '2-digit',
     second: '2-digit',
