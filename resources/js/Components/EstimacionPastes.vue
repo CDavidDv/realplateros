82bf7446063737385f6304e0ecb38260f68cba75
CDavidDv <carlos.david1806@gmail.com>
2025-05-19 09:39:51 -0600

    Actualiza el archivo manifest.json para reflejar cambios en los nombres de los archivos de JavaScript generados. Se eliminan archivos obsoletos relacionados con componentes de Vue, incluyendo ActionMessage, ApiTokenManager y otros, optimizando la estructura del proyecto. Se ajusta el rango de horas disponibles en el componente EstimacionPastes, ampliando el horario de 7:00 am a 9:00 pm.

--- resources/js/Components/EstimacionPastes.vue
+++ resources/js/Components/EstimacionPastes.vue
@@ -91,7 +91,7 @@ const days = [
 ]
 //de hora en hora de 9:00 am a 9:00 pm
 
-const hours = ['9:00 am', '10:00 am', '11:00 am', '12:00 pm', '1:00 pm', '2:00 pm', '3:00 pm', '4:00 pm', '5:00 pm', '6:00 pm', '7:00 pm', '8:00 pm', '9:00 pm']
+const hours = ['7:00 am', '8:00 am', '9:00 am', '10:00 am', '11:00 am', '12:00 pm', '1:00 pm', '2:00 pm', '3:00 pm', '4:00 pm', '5:00 pm', '6:00 pm', '7:00 pm', '8:00 pm', '9:00 pm']
 const products = computed(() => 
   props.inventario
     .filter(item => ['pastes', 'empanadas dulces', 'empanadas saladas'].includes(item.tipo))
