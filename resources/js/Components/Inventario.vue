1926c94e6c99c9a56ff1eb8254281c0fef634662
CDavidDv <carlos.david1806@gmail.com>
2025-07-18 20:13:17 -0600

    fix

--- resources/js/Components/Inventario.vue
+++ resources/js/Components/Inventario.vue
@@ -212,7 +212,7 @@
                     <button
                         @click="closeModal"
                         class="px-4 py-2 bg-gray-200 hover:bg-gray-300 text-gray-700 rounded-lg">
-                        Cancelar
+                        Cancelar-
                     </button>
                     <button
                         @click="subsCategory"
